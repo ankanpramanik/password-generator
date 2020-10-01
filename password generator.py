670c03b07c8cd2342ded38393b699f01c592fa63
Ankan Pramanik <72189396+ankanpramanik@users.noreply.github.com>
2020-10-01 13:23:24 +0530

    Add files via upload

---
+++ password generator.py
@@ -0,0 +1,43 @@
+import random as ran
+
+n=int(input("Enter the length of your password (max 16 char): "))
+
+if n<=16:
+  alphaupper1=chr(ran.randint(65,90))
+  alphaupper2=chr(ran.randint(65,90))
+  alphaupper3=chr(ran.randint(65,90))
+  alphaupper4=chr(ran.randint(65,90))
+  alphalower1=chr(ran.randint(97,122))
+  alphalower2=chr(ran.randint(97,122))
+  alphalower3=chr(ran.randint(97,122))
+  alphalower4=chr(ran.randint(97,122))
+  num1=chr(ran.randint(48,57))
+  num2=chr(ran.randint(48,57))
+  num3=chr(ran.randint(48,57))
+  num4=chr(ran.randint(48,57))
+  splchar1=chr(ran.randint(33,47))
+  splchar2=chr(ran.randint(33,47))
+  splchar3=chr(ran.randint(33,47))
+  splchar4=chr(ran.randint(33,47))
+
+  def shuffle(string):
+    tempList = list(string)
+    ran.shuffle(tempList)
+    return ''.join(tempList)
+
+  password=alphaupper1+alphaupper2+alphalower2+alphalower1+num1+num2+splchar1+splchar2+alphaupper3+alphaupper4+alphalower3+alphalower4+num3+num4+splchar3+splchar4
+
+  password=shuffle(password)
+  pw=password[:n]
+
+  print("Your suggested password is:",pw)
+  if len(pw)<8:
+    print("Strenth: Weak")
+  elif len(pw)==8:
+    print("Strenth: Moderate")
+  elif len(pw)>8:
+    print("Strenth: Strong")
+
+else:
+  print("Wrong input")
+
